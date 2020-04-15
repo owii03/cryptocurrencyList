6ff41077030117519adeada5679aa00a8708e597
Owii <oiichrism03@gmail.com>
2020-04-15 18:29:32 +0700

    Initial Commit

---
+++ Crypto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Crypto
+//
+//  Created by Owii on 15/04/20.
+//  Copyright © 2020 DataEdukasi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
